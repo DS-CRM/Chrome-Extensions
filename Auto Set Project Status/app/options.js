4948a30f6eb38fae58b4d4c584d973db99a6d362
rohit.kumar1@dotsquares.com <6QWzZo+5gT12LJljYz2sPw==>
2016-09-23 17:21:23 +0530

    Initial Commit

---
+++ Auto Set Project Status/app/options.js
@@ -0,0 +1,102 @@
+var projectOptions = [];
+// Saves options to chrome.storage
+function save_options() {
+    var optionlabel = document.getElementById('optionlabel').value;
+    var optionvalue = document.getElementById('optionvalue').value;
+    restore_options();
+    projectOptions.push({ label: optionlabel, value: optionvalue });
+    chrome.storage.sync.set({
+        projectOptions: projectOptions
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        document.getElementById('optionlabel').value = '';
+        document.getElementById('optionvalue').value = '';
+        restore_options();
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+        projectOptions: []
+    }, function (items) {
+        projectOptions = items.projectOptions;
+        refershprojectoptions();
+        var val = '';
+        for (var i = 0; i < projectOptions.length; i++) {
+            val += projectOptions[i].label + '<br />';
+        }
+        document.getElementById('itemlist').innerHTML = val;
+    });
+}
+
+function clear_options() {
+    restore_options();
+
+    chrome.storage.sync.set({
+        projectOptions: []
+    }, function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'All options deleted.';
+        restore_options();
+        setTimeout(function () {
+            status.textContent = '';
+        }, 750);
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    restore_options();
+});
+
+function refershprojectoptions() {
+    var bodyitems = document.getElementById('bodyitems');
+    if (bodyitems && projectOptions) {
+        while (bodyitems.firstChild) {
+            bodyitems.removeChild(bodyitems.firstChild);
+        }
+        if (projectOptions.length > 1) {
+            for (var i = 0; i < projectOptions.length; i++) {
+                var node = document.createElement("DIV");
+                var att = document.createAttribute("id");
+                att.value = projectOptions[i].value;
+                node.setAttributeNode(att);
+                var t = document.createTextNode(projectOptions[i].label);
+                node.appendChild(t);
+                bodyitems.appendChild(node);
+            }
+            var divs = document.querySelectorAll('div');
+            for (var i = 0; i < divs.length; i++) {
+                divs[i].addEventListener('click', click);
+            }
+        } else if (projectOptions.length == 1) {
+            chrome.tabs.executeScript(null, { file: "jquery_1.8.3.min.js" }, function () {
+                chrome.tabs.executeScript({
+                    code: '$("#cbProjectsList").val("' + projectOptions[i].value + '");$("#pbGo").removeAttr("disabled");$("#pbGo").trigger("click");'
+                });
+                window.close();
+            });
+        }
+    }
+}
+
+
+function click(e) {
+    chrome.tabs.executeScript(null, { file: "jquery_1.8.3.min.js" }, function () {
+        chrome.tabs.executeScript({
+            code: '$("#cbProjectsList").val("' + e.target.id + '");$("#pbGo").removeAttr("disabled");$("#pbGo").trigger("click");'
+        });
+        window.close();
+    });
+}
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('clear').addEventListener('click', clear_options);
